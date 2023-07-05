39edbd578536cc3f95f53df9dd0608eeffc8368a
AnastasiiaTurbal <138533538+AnastasiiaTurbal@users.noreply.github.com>
2023-07-05 21:22:26 +0300

    Update RomToArab.java

--- src/RomToArab.java
+++ src/RomToArab.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public enum RomToArab {
+public enum RomToArab {
+    I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10), XX(20), XXX(30), XL(40), L(50), LX(60), LXX(70), LXXX(80), XC(90), C(100);
+    private int value;
+    RomToArab(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
 }
